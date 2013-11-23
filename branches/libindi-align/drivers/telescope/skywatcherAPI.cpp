5c335f65939a1d95d1f7b4e0ff963778bef24060
rogerjames99 <rogerjames99@14e3e35b-4bd0-4918-bbf4-08149c233275>
2013-11-23 15:30:48 +0000

    Started work on a proof of concept driver for the alignment subsystem

---
+++ branches/libindi-align/drivers/telescope/skywatcherAPI.cpp
@@ -0,0 +1,375 @@
+/*!
+ * \file skywatcherAPI.cpp
+ *
+ * \author Roger James
+ * \author Gerry Rozema
+ * \author Jean-Luc Geehalel
+ * \date 13th November 2013
+ *
+ * This file contains the implementation in C++ of the Skywatcher API.
+ * It is based on work from three sources.
+ * A C++ implementation of the API by Roger James.
+ * The indi_eqmod driver by Jean-Luc Geehalel.
+ * The synscanmount driver by Gerry Rozema.
+ */
+
+#include "skywatcherAPI.h"
+
+#include <memory>
+#include <cmath>
+#include <cstdio>
+
+void AXISSTATUS::SetFullStop()
+{
+    FullStop = true;
+    SlewingTo = Slewing = false;
+}
+
+void AXISSTATUS::SetSlewing(bool forward, bool highspeed)
+{
+    FullStop = SlewingTo = false;
+    Slewing = true;
+
+    SlewingForward = forward;
+    HighSpeed = highspeed;
+}
+
+void AXISSTATUS::SetSlewingTo(bool forward, bool highspeed)
+{
+    FullStop = Slewing = false;
+    SlewingTo = true;
+
+    SlewingForward = forward;
+    HighSpeed = highspeed;
+}
+
+SkywatcherAPI::SkywatcherAPI()
+{
+    MCVersion = 0;
+    FactorStepToRad[AXIS1] = FactorStepToRad[AXIS2] = 0;
+    FactorRadToStep[AXIS1] = FactorRadToStep[AXIS2] = 0;
+    Positions[AXIS1] = Positions[AXIS2] = 0;
+}
+
+SkywatcherAPI::~SkywatcherAPI()
+{
+    //dtor
+}
+
+bool SkywatcherAPI::TalkWithAxis(AXISID Axis, char Command, std::string& cmdDataStr, std::string& responseStr)
+{
+
+    std::string SendBuffer;
+    int bytesWritten;
+    int bytesRead;
+    bool StartReading = false;
+    bool EndReading = false;
+
+    SendBuffer.push_back(':');
+    SendBuffer.push_back(Command);
+    SendBuffer.push_back(Axis == AXIS1 ? '1' : '2');
+    SendBuffer.append(cmdDataStr);
+    SendBuffer.push_back('\r');
+    skywatcher_tty_write(MyPortFD, SendBuffer.c_str(), SendBuffer.size(), &bytesWritten);
+
+    while (!EndReading)
+    {
+        char c;
+
+        int rc = skywatcher_tty_read(MyPortFD, &c, 1, 10, &bytesRead);
+        if ((rc != TTY_OK) || (bytesRead != 1))
+            return false;
+
+        if ((c == '=') || (c == '!'))
+            StartReading = true;
+
+        if (StartReading)
+            responseStr.push_back(c);
+
+        if ((c == '\r') && StartReading)
+            EndReading = true;
+    }
+    return true;
+}
+
+long SkywatcherAPI::BCDstr2long(std::string &string)
+{
+// =020782 => 8521474
+    // Funny BCD :-) string is pairs of hex chars with each pair representing a 8 bit hex number. The whole
+    // string being treated as least significant heg digit pair first!
+    const char *str = string.c_str();
+    long value = 0;
+    for (int i = 0; i < string.length(); i += 2)
+    {
+        long hexpair;
+        sscanf(str + i, "%2lx", &hexpair);
+        value += hexpair << i * 4;
+    }
+    return value;
+}
+
+long SkywatcherAPI::AngleToStep(AXISID Axis, double AngleInRad)
+{
+    return (long)(AngleInRad * FactorRadToStep[(int)Axis]);
+}
+
+double SkywatcherAPI::StepToAngle(AXISID Axis, long Steps)
+{
+    return Steps * FactorStepToRad[(int)Axis];
+}
+
+long SkywatcherAPI::RadSpeedToInt(AXISID Axis, double RateInRad)
+{
+    return (long)(RateInRad * FactorRadRateToInt[(int)Axis]);
+}
+
+bool SkywatcherAPI::CheckIfDCMotor()
+{
+	// Flush the tty read buffer
+	char input[20];
+	int rc;
+	int nbytes;
+
+	while (true)
+	{
+		rc =  skywatcher_tty_read(MyPortFD, input, 20, 5, &nbytes);
+		if (TTY_TIME_OUT == rc)
+			break;
+		if (TTY_OK != rc)
+			return false;
+	}
+
+	if (TTY_OK != skywatcher_tty_write(MyPortFD, ":", 1, &nbytes))
+		return false;
+
+	rc =  skywatcher_tty_read(MyPortFD, input, 1, 5, &nbytes);
+
+	if ((TTY_OK == rc) && (1 == nbytes) && (':' == input[0]))
+	{
+		IsDCMotor = true;
+		return true;
+	}
+	if (TTY_TIME_OUT == rc)
+	{
+		IsDCMotor = false;
+		return true;
+	}
+
+	return false;
+}
+
+bool SkywatcherAPI::MCInit()
+{
+	if (!CheckIfDCMotor())
+		return false;
+
+    if (!InquireMotorBoardVersion(AXIS1))
+        return false;
+
+    MountCode = MCVersion & 0xFF;
+
+    //// NOTE: Simulator settings, Mount dependent Settings
+
+    // Inquire Gear Rate
+    if (!InquireGridPerRevolution(AXIS1))
+        return false;
+    if (!InquireGridPerRevolution(AXIS2))
+        return false;
+
+    // Inquire motor timer interrup frequency
+    if (!InquireTimerInterruptFreq(AXIS1))
+        return false;
+    if (!InquireTimerInterruptFreq(AXIS2))
+        return false;
+
+    // Inquire motor high speed ratio
+    if (!InquireHighSpeedRatio(AXIS1))
+        return false;
+    if (!InquireHighSpeedRatio(AXIS2))
+        return false;
+
+    // Inquire PEC period
+    // DC motor controller does not support PEC
+    if (!IsDCMotor)
+    {
+//        if (!InquirePECPeriod(AXIS1);
+//        if (!InquirePECPeriod(AXIS2);
+    }
+
+    // Inquire Axis Position
+    if (!MCGetAxisPosition(AXIS1))
+        return false;
+    if (!MCGetAxisPosition(AXIS2))
+        return false;
+
+    if (!InitializeMC())
+        return false;
+
+    // These two LowSpeedGotoMargin are calculate from slewing for 5 seconds in 128x sidereal rate
+    LowSpeedGotoMargin[(int)AXIS1] = (long)(640 * CONSTANT::SIDEREALRATE * FactorRadToStep[(int)AXIS1]);
+    LowSpeedGotoMargin[(int)AXIS2] = (long)(640 * CONSTANT::SIDEREALRATE * FactorRadToStep[(int)AXIS2]);
+
+    // Default break steps
+    BreakSteps[(int)AXIS1] = 3500;
+    BreakSteps[(int)AXIS2] = 3500;
+
+    return true;
+}
+
+bool SkywatcherAPI::MCGetAxisPosition(AXISID Axis)
+{
+    std::string Parameters, Response;
+    if (!TalkWithAxis(Axis, 'j', Parameters, Response))
+    	return false;
+
+    long iPosition = BCDstr2long(Response);
+    iPosition -= 0x00800000;
+    Positions[(int)Axis] = StepToAngle(Axis, iPosition);
+
+    return true;
+}
+
+bool SkywatcherAPI::MCGetAxisStatus(AXISID Axis)
+{
+    std::string Parameters, Response;
+
+    if(!TalkWithAxis(Axis, 'f', Parameters, Response))
+        return false;
+
+    if ((Response[2] & 0x01) != 0)
+    {
+        // Axis is running
+        AxesStatus[(int)Axis].FullStop = false;
+        if ((Response[1] & 0x01) != 0)
+        {
+            AxesStatus[(int)Axis].Slewing = true;		// Axis in slewing(AstroMisc speed) mode.
+            AxesStatus[(int)Axis].SlewingTo = false;
+        }
+        else
+        {
+            AxesStatus[(int)Axis].SlewingTo = true;		// Axis in SlewingTo mode.
+            AxesStatus[(int)Axis].Slewing = false;
+        }
+    }
+    else
+    {
+
+        AxesStatus[(int)Axis].FullStop = true;	// FullStop = 1;	// Axis is fully stop.
+        AxesStatus[(int)Axis].Slewing = false;
+        AxesStatus[(int)Axis].SlewingTo = false;
+   }
+
+    if ((Response[1] & 0x02) == 0)
+        AxesStatus[(int)Axis].SlewingForward = true;	// Angle increase = 1;
+    else
+        AxesStatus[(int)Axis].SlewingForward = false;
+
+    if ((Response[1] & 0x04) != 0)
+        AxesStatus[(int)Axis].HighSpeed = true; // HighSpeed running mode = 1;
+    else
+        AxesStatus[(int)Axis].HighSpeed = false;
+
+    if ((Response[3] & 1) == 0)
+        AxesStatus[(int)Axis].NotInitialized = true;	// MC is not initialized.
+    else
+        AxesStatus[(int)Axis].NotInitialized = false;
+
+
+    return true;
+}
+
+/************************ MOTOR COMMAND SET ***************************/
+// Inquire Motor Board Version ":e(*1)", where *1: '1'= CH1, '2'= CH2, '3'= Both.
+bool SkywatcherAPI::InquireMotorBoardVersion(AXISID Axis)
+{
+    std::string Parameters, Response;
+
+    if (!TalkWithAxis(Axis, 'e', Parameters, Response))
+        return false;
+
+    long tmpMCVersion = BCDstr2long(Response);
+
+    MCVersion = ((tmpMCVersion & 0xFF) << 16) | ((tmpMCVersion & 0xFF00)) | ((tmpMCVersion & 0xFF0000) >> 16);
+
+    return true;
+}
+
+// Inquire Grid Per Revolution ":a(*2)", where *2: '1'= CH1, '2' = CH2.
+bool SkywatcherAPI::InquireGridPerRevolution(AXISID Axis)
+{
+    std::string Parameters, Response;
+
+    if (!TalkWithAxis(Axis, 'a', Parameters, Response))
+        return false;
+
+
+    long GearRatio = BCDstr2long(Response);
+
+    // There is a bug in the earlier version firmware(Before 2.00) of motor controller MC001.
+    // Overwrite the GearRatio reported by the MC for 80GT mount and 114GT mount.
+    if ((MCVersion & 0x0000FF) == 0x80)
+        GearRatio = 0x162B97;		// for 80GT mount
+    if ((MCVersion & 0x0000FF) == 0x82)
+        GearRatio = 0x205318;		// for 114GT mount
+
+    FactorRadToStep[(int)Axis] = GearRatio / (2 * M_PI);
+    FactorStepToRad[(int)Axis] = 2 * M_PI / GearRatio;
+
+    return true;
+}
+
+// Inquire Timer Interrupt Freq ":b1".
+bool SkywatcherAPI::InquireTimerInterruptFreq(AXISID Axis)
+{
+    std::string Parameters, Response;
+
+    if (!TalkWithAxis(Axis, 'b', Parameters, Response))
+        return false;
+
+    long TimeFreq = BCDstr2long(Response);
+    StepTimerFreq[(int)Axis] = TimeFreq;
+
+    FactorRadRateToInt[(int)Axis] = (double)(StepTimerFreq[(int)Axis]) / FactorRadToStep[(int)Axis];
+
+    return true;
+}
+
+// Inquire high speed ratio ":g(*2)", where *2: '1'= CH1, '2' = CH2.
+bool SkywatcherAPI::InquireHighSpeedRatio(AXISID Axis)
+{
+    std::string Parameters, Response;
+
+    if (!TalkWithAxis(Axis, 'g', Parameters, Response))
+        return false;
+
+    long highSpeedRatio = BCDstr2long(Response);
+    HighSpeedRatio[(int)Axis] = highSpeedRatio;
+
+    return true;
+}
+
+// Inquire PEC Period ":s(*1)", where *1: '1'= CH1, '2'= CH2, '3'= Both.
+bool SkywatcherAPI::InquirePECPeriod(AXISID Axis)
+{
+    std::string Parameters, Response;
+
+    if (!TalkWithAxis(Axis, 's', Parameters, Response))
+        return false;
+
+    long PECPeriod = BCDstr2long(Response);
+    PESteps[(int)Axis] = PECPeriod;
+
+    return true;
+}
+
+// Set initialization done ":F3", where '3'= Both CH1 and CH2.
+bool SkywatcherAPI::InitializeMC()
+{
+    std::string Parameters, Response;
+
+    if (!TalkWithAxis(AXIS1, 'F', Parameters, Response))
+        return false;
+    if (!TalkWithAxis(AXIS2, 'F', Parameters, Response))
+        return false;
+    return true;
+}
---
+++ branches/libindi-align/drivers/telescope/skywatcherAPI.h
@@ -0,0 +1,95 @@
+/*!
+ * \file skywatcherAPI.cpp
+ *
+ * \author Roger James
+ * \author Gerry Rozema
+ * \author Jean-Luc Geehalel
+ * \date 13th November 2013
+ *
+ * This file contains the definitions for a C++ implementatiom of the Skywatcher API.
+ * It is based on work from three sources.
+ * A C++ implementation of the API by Roger James.
+ * The indi_eqmod driver by Jean-Luc Geehalel.
+ * The synscanmount driver by Gerry Rozema.
+ */
+
+#ifndef SKYWATCHERAPI_H
+#define SKYWATCHERAPI_H
+
+#include <cmath>
+#include <string>
+
+class CONSTANT
+{
+    public:
+         static const double SIDEREALRATE = (2 * M_PI / 86164.09065);
+};
+
+struct AXISSTATUS
+{
+    AXISSTATUS() : FullStop(false), Slewing(false), SlewingTo(false), SlewingForward(false), HighSpeed(false), NotInitialized(true) {}
+    bool FullStop;
+    bool Slewing;
+    bool SlewingTo;
+    bool SlewingForward;
+    bool HighSpeed;
+    bool NotInitialized;
+
+    void SetFullStop();
+    void SetSlewing(bool forward, bool highspeed);
+    void SetSlewingTo(bool forward, bool highspeed);
+};
+
+class SkywatcherAPI
+{
+public:
+    SkywatcherAPI();
+    virtual ~SkywatcherAPI();
+
+    void SetSerialPort(int port) { MyPortFD = port; }
+
+    enum AXISID { AXIS1 = 0, AXIS2 = 1 };
+    bool TalkWithAxis(AXISID Axis, char Command, std::string& cmdDataStr, std::string& responseStr);
+    long BCDstr2long(std::string &string);
+    long AngleToStep(AXISID Axis, double AngleInRad);
+    double StepToAngle(AXISID Axis, long Steps);
+    long RadSpeedToInt(AXISID Axis, double RateInRad);
+    bool CheckIfDCMotor();
+    bool MCInit();
+    bool MCGetAxisPosition(AXISID Axis);
+    bool MCGetAxisStatus(AXISID Axis);
+    bool InquireMotorBoardVersion(AXISID Axis);
+    bool InquireGridPerRevolution(AXISID Axis);
+    bool InquireTimerInterruptFreq(AXISID Axis);
+    bool InquireHighSpeedRatio(AXISID Axis);
+    bool InquirePECPeriod(AXISID Axis);
+    bool InitializeMC();
+
+    // Skywatcher mount status variables
+    long MCVersion; // Motor control board firmware version
+    long MountCode;
+    bool IsDCMotor;
+    double FactorRadToStep[2];
+    double FactorStepToRad[2];
+    double FactorRadRateToInt[2];
+    AXISSTATUS AxesStatus[2];
+    long StepTimerFreq[2];
+    long HighSpeedRatio[2];
+    long PESteps[2];
+    double Positions[2];
+    long LowSpeedGotoMargin[2];
+    long BreakSteps[2];
+private:
+    enum TTY_ERROR { TTY_OK=0, TTY_READ_ERROR=-1, TTY_WRITE_ERROR=-2, TTY_SELECT_ERROR=-3, TTY_TIME_OUT=-4, TTY_PORT_FAILURE=-5, TTY_PARAM_ERROR=-6, TTY_ERRNO = -7};
+    virtual int skywatcher_tty_read(int fd, char *buf, int nbytes, int timeout, int *nbytes_read) = 0;
+//    virtual int skywatcher_tty_read_section(int fd, char *buf, char stop_char, int timeout, int *nbytes_read) = 0;
+    virtual int skywatcher_tty_write(int fd, const char * buffer, int nbytes, int *nbytes_written) = 0;
+//    virtual int skywatcher_tty_write_string(int fd, const char * buffer, int *nbytes_written) = 0;
+//    virtual int skywatcher_tty_connect(const char *device, int bit_rate, int word_size, int parity, int stop_bits, int *fd) = 0;
+//    virtual int skywatcher_tty_disconnect(int fd) = 0;
+//    virtual void skywatcher_tty_error_msg(int err_code, char *err_msg, int err_msg_len) = 0;
+//    virtual int skywatcher_tty_timeout(int fd, int timeout) = 0;*/
+    int MyPortFD;
+};
+
+#endif // SKYWATCHERAPI_H
