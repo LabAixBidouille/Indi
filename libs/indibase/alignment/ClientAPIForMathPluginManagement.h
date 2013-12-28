6c317e0835dcf665c1d75e37012707b5b0c46d77
rogerjames99 <rogerjames99@14e3e35b-4bd0-4918-bbf4-08149c233275>
2013-12-28 17:46:11 +0000

    Finished plugin management stuff. Also added a client to test it.

--- libs/indibase/alignment/ClientAPIForMathPluginManagement.h
+++ libs/indibase/alignment/ClientAPIForMathPluginManagement.h
@@ -12,6 +12,9 @@
 #include <vector>
 #include <string>
 
+#include "indibase/basedevice.h"
+#include "indibase/baseclient.h"
+
 namespace INDI {
 namespace AlignmentSubsystem {
 
@@ -27,12 +30,37 @@ public:
 
     typedef std::vector<std::string> MathPluginsList;
 
+    void Initialise(INDI::BaseClient *BaseClient);
+
+    /** \brief Process new device message from driver. This routine should be called from within
+     the newDevice handler in the client. This routine is not normally called directly but is called by
+     the ProcessNewDevice function in INDI::Alignment::AlignmentSubsystemForClients which filters out calls
+     from unwanted devices. TODO maybe hide this function.
+        \param[in] DevicePointer A pointer to the INDI::BaseDevice object.
+    */
+    void ProcessNewDevice(INDI::BaseDevice *DevicePointer);
+
+    /** \brief Process new property message from driver. This routine should be called from within
+     the newProperty handler in the client. This routine is not normally called directly but is called by
+     the ProcessNewProperty function in INDI::Alignment::AlignmentSubsystemForClients which filters out calls
+     from unwanted devices. TODO maybe hide this function.
+        \param[in] PropertyPointer A pointer to the INDI::Property object.
+    */
+    void ProcessNewProperty(INDI::Property *PropertyPointer);
+
+    /** \brief Process new switch message from driver. This routine should be called from within
+     the newSwitch handler in the client. This routine is not normally called directly but is called by
+     the ProcessNewSwitch function in INDI::Alignment::AlignmentSubsystemForClients which filters out calls
+     from unwanted devices. TODO maybe hide this function.
+        \param[in] SwitchVectorProperty A pointer to the INDI::ISwitchVectorProperty.
+    */
+    void ProcessNewSwitch(ISwitchVectorProperty *SwitchVectorProperty);
     /*!
      * \brief Return a list of the names of the available math plugins.
-     * \param[out] MathPlugins Reference to a list of the names of the available math plugins.
+     * \param[out] AvailableMathPlugins Reference to a list of the names of the available math plugins.
      * \return False on failure
      */
-    bool EnumerateMathPlugins(MathPluginsList& MathPlugins);
+    bool EnumerateMathPlugins(MathPluginsList& AvailableMathPlugins);
 
     /*!
      * \brief Selects, loads and initialises the named math plugin.
@@ -40,6 +68,29 @@ public:
      * \return False on failure.
      */
     bool SelectMathPlugin(const std::string& MathPluginName);
+
+    /*!
+     * \brief Re-initialises the current math plugin.
+     * \return False on failure.
+     */
+    bool ReInitialiseMathPlugin();
+
+private:
+    // Pointer to INDI::BaseClient
+    INDI::BaseClient *BaseClient;
+
+    // Synchronise with the listener thread
+    bool WaitForDriverCompletion();
+    bool SignalDriverCompletion();
+    bool SetDriverBusy();
+    pthread_cond_t DriverActionCompleteCondition;
+    pthread_mutex_t DriverActionCompleteMutex;
+    bool DriverActionComplete;
+
+    INDI::BaseDevice *Device;
+    INDI::Property *MathPlugins;
+    INDI::Property *PluginInitialise;
+
 };
 
 } // namespace AlignmentSubsystem
