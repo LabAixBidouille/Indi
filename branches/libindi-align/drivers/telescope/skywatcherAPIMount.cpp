908425407df0f8b47d281f9840d3ced8fc384661
rogerjames99 <rogerjames99@14e3e35b-4bd0-4918-bbf4-08149c233275>
2013-12-30 14:47:46 +0000

    Small fix to the blob support

--- branches/libindi-align/drivers/telescope/skywatcherAPIMount.cpp
+++ branches/libindi-align/drivers/telescope/skywatcherAPIMount.cpp
@@ -62,15 +62,10 @@ void ISNewNumber(const char *dev, const char *name, double values[], char *names
 
 void ISNewBLOB (const char *dev, const char *name, int sizes[], int blobsizes[], char *blobs[], char *formats[], char *names[], int n)
 {
-    INDI_UNUSED(dev);
-    INDI_UNUSED(name);
-    INDI_UNUSED(sizes);
-    INDI_UNUSED(blobsizes);
-    INDI_UNUSED(blobs);
-    INDI_UNUSED(formats);
-    INDI_UNUSED(names);
-    INDI_UNUSED(n);
+    ISInit();
+    SkywatcherAPIMountPtr->ISNewBLOB (dev, name, sizes, blobsizes, blobs, formats, names, n);
 }
+
 void ISSnoopDevice (XMLEle *root)
 {
     INDI_UNUSED(root);
