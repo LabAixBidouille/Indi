dbfb218a732d59c03f11ca09ab062e33b83018c5
rogerjames99 <rogerjames99@14e3e35b-4bd0-4918-bbf4-08149c233275>
2014-01-06 19:02:43 +0000

    Started work on completing SkywatcherAPIMount driver.

--- branches/libindi-align/drivers/telescope/skywatcherAPIMount.cpp
+++ branches/libindi-align/drivers/telescope/skywatcherAPIMount.cpp
@@ -18,6 +18,7 @@
 
 #include <memory>
 #include <cmath>
+#include <limits>
 
 // We declare an auto pointer to SkywatcherAPIMount.
 std::auto_ptr<SkywatcherAPIMount> SkywatcherAPIMountPtr(0);
@@ -71,6 +72,7 @@ void ISSnoopDevice (XMLEle *root)
     INDI_UNUSED(root);
 }
 
+const char * SkywatcherAPIMount::DetailedMountInfoPage = "Detailed Mount Information";
 
 SkywatcherAPIMount::SkywatcherAPIMount()
 {
@@ -148,25 +150,217 @@ bool SkywatcherAPIMount::initProperties()
     // Add alignment properties
     InitProperties(this);
 
-    // Add my visible before connection properties if any
+    // Set up property variables
+    IUFillNumber(&BasicMountInfo[MOTOR_CONTROL_FIRMWARE_VERSION], "MOTOR_CONTROL_FIRMWARE_VERSION",
+                                                            "Motor control fimware version",
+                                                            "%g",
+                                                            0,
+                                                            0xFFFFFF,
+                                                            1,
+                                                            0);
+    IUFillNumber(&BasicMountInfo[MOUNT_CODE], "MOUNT_CODE",
+                                                            "Mount code",
+                                                            "%g",
+                                                            0,
+                                                            0xFF,
+                                                            1,
+                                                            0);
+    IUFillNumber(&BasicMountInfo[IS_DC_MOTOR], "IS_DC_MOTOR",
+                                                            "Is DC motor (boolean)",
+                                                            "%g",
+                                                            0,
+                                                            1,
+                                                            1,
+                                                            0);
+    IUFillNumberVector(&BasicMountInfoV, BasicMountInfo, 3, getDeviceName(), "BASIC_MOUNT_INFO", "Basic mount information",
+                        DetailedMountInfoPage, IP_RO, 60, IPS_IDLE);
 
+    IUFillSwitch(&MountType[MT_EQ6], "EQ6", "EQ6", ISS_OFF);
+    IUFillSwitch(&MountType[MT_HEQ5], "HEQ5", "HEQ5", ISS_OFF);
+    IUFillSwitch(&MountType[MT_EQ5], "EQ5", "EQ5", ISS_OFF);
+    IUFillSwitch(&MountType[MT_EQ3], "EQ3", "EQ3", ISS_OFF);
+    IUFillSwitch(&MountType[MT_GT], "GT", "GT", ISS_OFF);
+    IUFillSwitch(&MountType[MT_MF], "MF", "MF", ISS_OFF);
+    IUFillSwitch(&MountType[MT_114GT], "114GT", "114GT", ISS_OFF);
+    IUFillSwitch(&MountType[MT_DOB], "DOB", "DOB", ISS_OFF);
+    IUFillSwitch(&MountType[MT_UNKNOWN], "UNKNOWN", "UNKNOWN", ISS_ON);
+    IUFillSwitchVector(&MountTypeV, MountType, 9, getDeviceName(), "MOUNT_TYPE", "Mount type", DetailedMountInfoPage, IP_RO,
+                        ISR_ATMOST1, 60, IPS_IDLE);
+
+    IUFillNumber(&AxisOneInfo[GEAR_RATIO], "GEAR_RATIO",
+                                                            "Gear Ratio",
+                                                            "%g",
+                                                            0,
+                                                            0xFFFFFF,
+                                                            1,
+                                                            0);
+    IUFillNumber(&AxisOneInfo[STEP_TIMER_FREQUENCY], "STEP_TIMER_FREQUENCY",
+                                                            "Step timer frequency",
+                                                            "%g",
+                                                            0,
+                                                            0xFFFFFF,
+                                                            1,
+                                                            0);
+    IUFillNumber(&AxisOneInfo[HIGH_SPEED_RATIO], "HIGH_SPEED_RATIO",
+                                                            "High speed ratio",
+                                                            "%g",
+                                                            0,
+                                                            0xFFFFFF,
+                                                            1,
+                                                            0);
+    IUFillNumber(&AxisOneInfo[PE_PERIOD], "PE_PERIOD",
+                                                            "PE period",
+                                                            "%g",
+                                                            0,
+                                                            0xFFFFFF,
+                                                            1,
+                                                            0);
+
+    IUFillNumberVector(&AxisOneInfoV, AxisOneInfo, 4, getDeviceName(), "AXIS_ONE_INFO", "Axis one information",
+                        DetailedMountInfoPage, IP_RO, 60, IPS_IDLE);
+
+    IUFillSwitch(&AxisOneState[FULL_STOP], "FULL_STOP", "FULL_STOP", ISS_OFF);
+    IUFillSwitch(&AxisOneState[SLEWING], "SLEWING", "SLEWING", ISS_OFF);
+    IUFillSwitch(&AxisOneState[SLEWING_TO], "SLEWING_TO", "SLEWING_TO", ISS_OFF);
+    IUFillSwitch(&AxisOneState[SLEWING_FORWARD], "SLEWING_FORWARD", "SLEWING_FORWARD", ISS_OFF);
+    IUFillSwitch(&AxisOneState[HIGH_SPEED], "HIGH_SPEED", "HIGH_SPEED", ISS_OFF);
+    IUFillSwitch(&AxisOneState[NOT_INITIALISED], "NOT_INITIALISED", "NOT_INITIALISED", ISS_ON);
+    IUFillSwitchVector(&AxisOneStateV, AxisOneState, 6, getDeviceName(), "AXIS_ONE_STATE", "Axis one state", DetailedMountInfoPage, IP_RO,
+                        ISR_NOFMANY, 60, IPS_IDLE);
+
+    IUFillNumber(&AxisTwoInfo[GEAR_RATIO], "GEAR_RATIO",
+                                                            "Gear Ratio",
+                                                            "%g",
+                                                            0,
+                                                            0xFFFFFF,
+                                                            1,
+                                                            0);
+    IUFillNumber(&AxisTwoInfo[STEP_TIMER_FREQUENCY], "STEP_TIMER_FREQUENCY",
+                                                            "Step timer frequency",
+                                                            "%g",
+                                                            0,
+                                                            0xFFFFFF,
+                                                            1,
+                                                            0);
+    IUFillNumber(&AxisTwoInfo[HIGH_SPEED_RATIO], "HIGH_SPEED_RATIO",
+                                                            "High speed ratio",
+                                                            "%g",
+                                                            0,
+                                                            0xFFFFFF,
+                                                            1,
+                                                            0);
+    IUFillNumber(&AxisTwoInfo[PE_PERIOD], "PE_PERIOD",
+                                                            "PE period",
+                                                            "%g",
+                                                            0,
+                                                            0xFFFFFF,
+                                                            1,
+                                                            0);
+
+    IUFillNumberVector(&AxisTwoInfoV, AxisTwoInfo, 4, getDeviceName(), "AXIS_TWO_INFO", "Axis two information",
+                        DetailedMountInfoPage, IP_RO, 60, IPS_IDLE);
+
+    IUFillSwitch(&AxisTwoState[FULL_STOP], "FULL_STOP", "FULL_STOP", ISS_OFF);
+    IUFillSwitch(&AxisTwoState[SLEWING], "SLEWING", "SLEWING", ISS_OFF);
+    IUFillSwitch(&AxisTwoState[SLEWING_TO], "SLEWING_TO", "SLEWING_TO", ISS_OFF);
+    IUFillSwitch(&AxisTwoState[SLEWING_FORWARD], "SLEWING_FORWARD", "SLEWING_FORWARD", ISS_OFF);
+    IUFillSwitch(&AxisTwoState[HIGH_SPEED], "HIGH_SPEED", "HIGH_SPEED", ISS_OFF);
+    IUFillSwitch(&AxisTwoState[NOT_INITIALISED], "NOT_INITIALISED", "NOT_INITIALISED", ISS_ON);
+    IUFillSwitchVector(&AxisTwoStateV, AxisTwoState, 6, getDeviceName(), "AXIS_TWO_STATE", "Axis two state", DetailedMountInfoPage, IP_RO,
+                        ISR_NOFMANY, 60, IPS_IDLE);
+
+    IUFillNumber(&EncoderValues[0], "AXIS_ONE",
+                                                            "Axis One",
+                                                            "%.0f",
+                                                            0,
+                                                            0xFFFFFF,
+                                                            1,
+                                                            0);
+    IUFillNumber(&EncoderValues[1], "AXIS_TWO",
+                                                            "Axis Two",
+                                                            "%.0f",
+                                                            0,
+                                                            0xFFFFFF,
+                                                            1,
+                                                            0);
+
+    IUFillNumberVector(&EncoderValuesV, EncoderValues, 2, getDeviceName(), "ENCODER_VALUES", "Encoder values",
+                        DetailedMountInfoPage, IP_RO, 60, IPS_IDLE);
+    // Register any visible before connection properties
 
     return true;
 }
+
 void SkywatcherAPIMount::ISGetProperties (const char *dev)
 {
-    // This iverride is currently here for debugging purposes only
     IDLog("SkywatcherAPIMount::ISGetProperties\n");
     INDI::Telescope::ISGetProperties(dev);
+
+    if (isConnected())
+    {
+        // Fill in any real values now available MCInit should have been called already
+        UpdateDetailedMountInformation(false);
+
+        // Define our connected only properties to the base driver
+        // e.g. defineNumber(MyNumberVectorPointer);
+        // This will register our properties and send a IDDefXXXX mewssage to any connected clients
+        defineNumber(&BasicMountInfoV);
+        defineSwitch(&MountTypeV);
+        defineNumber(&AxisOneInfoV);
+        defineSwitch(&AxisOneStateV);
+        defineNumber(&AxisTwoInfoV);
+        defineSwitch(&AxisTwoStateV);
+        defineNumber(&EncoderValuesV);
+    }
     return;
 }
+
+bool SkywatcherAPIMount::updateProperties()
+{
+    INDI::Telescope::updateProperties();
+
+    if (isConnected())
+    {
+        // Fill in any real values now available MCInit should have been called already
+        UpdateDetailedMountInformation(false);
+
+        // Define our connected only properties to the base driver
+        // e.g. defineNumber(MyNumberVectorPointer);
+        // This will register our properties and send a IDDefXXXX mewssage to any connected clients
+        // I have now idea why I have to do this here as well as in ISGetProperties. It makes me
+        // concerned there is a design or implementation flaw somewhere.
+        defineNumber(&BasicMountInfoV);
+        defineSwitch(&MountTypeV);
+        defineNumber(&AxisOneInfoV);
+        defineSwitch(&AxisOneStateV);
+        defineNumber(&AxisTwoInfoV);
+        defineSwitch(&AxisTwoStateV);
+        defineNumber(&EncoderValuesV);
+
+        // Start the timer if we need one
+        // SetTimer(POLLMS);
+    }
+    else
+    {
+        // Delete any connected only properties from the base driver's list
+        // e.g. deleteProperty(MyNumberVector.name);
+        deleteProperty(BasicMountInfoV.name);
+        deleteProperty(MountTypeV.name);
+        deleteProperty(AxisOneInfoV.name);
+        deleteProperty(AxisOneStateV.name);
+        deleteProperty(AxisTwoInfoV.name);
+        deleteProperty(AxisTwoStateV.name);
+        deleteProperty(EncoderValuesV.name);
+    }
+}
+
 
 bool SkywatcherAPIMount::ReadScopeStatus()
 {
     DEBUG(INDI::Logger::DBG_SESSION, "SkywatcherAPIMount::ReadScopeStatus");
 
     // Horrible hack to get over the fact that the base class calls ReadScopeStatus from inside Connect
-    // before I have a chanced to set up tth serial port
+    // before I have a chance to set up the serial port
     SetSerialPort(PortFD);
 
     // leave the following stuff in for the time being it is mostly harmless
@@ -244,3 +438,197 @@ int SkywatcherAPIMount::skywatcher_tty_write(int fd, const char * buffer, int nb
 {
     return tty_write(fd, buffer, nbytes, nbytes_written);
 }
+
+void SkywatcherAPIMount::UpdateDetailedMountInformation(bool InformClient)
+{
+    bool BasicMountInfoHasChanged = false;
+    if (BasicMountInfo[MOTOR_CONTROL_FIRMWARE_VERSION].value != MCVersion)
+    {
+        BasicMountInfo[MOTOR_CONTROL_FIRMWARE_VERSION].value = MCVersion;
+        BasicMountInfoHasChanged = true;
+    }
+    if (BasicMountInfo[MOUNT_CODE].value != MountCode)
+    {
+        BasicMountInfo[MOUNT_CODE].value = MountCode;
+        BasicMountInfoHasChanged = true;
+    }
+    if (BasicMountInfo[IS_DC_MOTOR].value != IsDCMotor)
+    {
+        BasicMountInfo[IS_DC_MOTOR].value = IsDCMotor;
+        BasicMountInfoHasChanged = true;
+    }
+    if (BasicMountInfoHasChanged && InformClient)
+        IDSetNumber(&BasicMountInfoV, NULL);
+
+    int OldMountType = IUFindOnSwitchIndex(&MountTypeV);
+    int NewMountType;
+    switch (MountCode)
+    {
+        case 0x00:
+            NewMountType = MT_EQ6;
+            break;
+        case 0x01:
+            NewMountType = MT_HEQ5;
+            break;
+        case 0x02:
+            NewMountType = MT_EQ5;
+            break;
+        case 0x03:
+            NewMountType = MT_EQ3;
+            break;
+        case 0x80:
+            NewMountType = MT_GT;
+            break;
+        case 0x81:
+            NewMountType = MT_MF;
+            break;
+        case 0x82:
+            NewMountType = MT_114GT;
+            break;
+        case 0x90:
+            NewMountType = MT_DOB;
+            break;
+        default:
+            NewMountType = MT_UNKNOWN;
+            break;
+    }
+    if (OldMountType != NewMountType)
+    {
+        IUResetSwitch(&MountTypeV);
+        MountType[NewMountType].s = ISS_ON;
+        if (InformClient)
+            IDSetSwitch(&MountTypeV, NULL);
+    }
+
+    bool AxisOneInfoHasChanged = false;
+    if (AxisOneInfo[GEAR_RATIO].value != GearRatio[0])
+    {
+        AxisOneInfo[GEAR_RATIO].value = GearRatio[0];
+        AxisOneInfoHasChanged = true;
+    }
+    if (AxisOneInfo[STEP_TIMER_FREQUENCY].value != StepTimerFreq[0])
+    {
+        AxisOneInfo[STEP_TIMER_FREQUENCY].value = StepTimerFreq[0];
+        AxisOneInfoHasChanged = true;
+    }
+    if (AxisOneInfo[HIGH_SPEED_RATIO].value != HighSpeedRatio[0])
+    {
+        AxisOneInfo[HIGH_SPEED_RATIO].value = HighSpeedRatio[0];
+        AxisOneInfoHasChanged = true;
+    }
+    if (AxisOneInfo[PE_PERIOD].value != PESteps[0])
+    {
+        AxisOneInfo[PE_PERIOD].value = PESteps[0];
+        AxisOneInfoHasChanged = true;
+    }
+    if (AxisOneInfoHasChanged && InformClient)
+        IDSetNumber(&AxisOneInfoV, NULL);
+
+    bool AxisOneStateHasChanged = false;
+    if (AxisOneState[FULL_STOP].s != (AxesStatus[0].FullStop ? ISS_ON : ISS_OFF))
+    {
+        AxisOneState[FULL_STOP].s = AxesStatus[0].FullStop ? ISS_ON : ISS_OFF;
+        AxisOneStateHasChanged = true;
+    }
+    if (AxisOneState[SLEWING].s != (AxesStatus[0].Slewing ? ISS_ON : ISS_OFF))
+    {
+        AxisOneState[SLEWING].s = AxesStatus[0].Slewing ? ISS_ON : ISS_OFF;
+        AxisOneStateHasChanged = true;
+    }
+    if (AxisOneState[SLEWING_TO].s != (AxesStatus[0].SlewingTo ? ISS_ON : ISS_OFF))
+    {
+        AxisOneState[SLEWING_TO].s = AxesStatus[0].SlewingTo ? ISS_ON : ISS_OFF;
+        AxisOneStateHasChanged = true;
+    }
+    if (AxisOneState[SLEWING_FORWARD].s != (AxesStatus[0].SlewingForward ? ISS_ON : ISS_OFF))
+    {
+        AxisOneState[SLEWING_FORWARD].s = AxesStatus[0].SlewingForward ? ISS_ON : ISS_OFF;
+        AxisOneStateHasChanged = true;
+    }
+    if (AxisOneState[HIGH_SPEED].s != (AxesStatus[0].HighSpeed ? ISS_ON : ISS_OFF))
+    {
+        AxisOneState[HIGH_SPEED].s = AxesStatus[0].HighSpeed ? ISS_ON : ISS_OFF;
+        AxisOneStateHasChanged = true;
+    }
+    if (AxisOneState[NOT_INITIALISED].s != (AxesStatus[0].NotInitialized ? ISS_ON : ISS_OFF))
+    {
+        AxisOneState[NOT_INITIALISED].s = AxesStatus[0].NotInitialized ? ISS_ON : ISS_OFF;
+        AxisOneStateHasChanged = true;
+    }
+    if (AxisOneStateHasChanged && InformClient)
+            IDSetSwitch(&AxisOneStateV, NULL);
+
+    bool AxisTwoInfoHasChanged = false;
+    if (AxisTwoInfo[GEAR_RATIO].value != GearRatio[1])
+    {
+        AxisTwoInfo[GEAR_RATIO].value = GearRatio[1];
+        AxisTwoInfoHasChanged = true;
+    }
+    if (AxisTwoInfo[STEP_TIMER_FREQUENCY].value != StepTimerFreq[1])
+    {
+        AxisTwoInfo[STEP_TIMER_FREQUENCY].value = StepTimerFreq[1];
+        AxisTwoInfoHasChanged = true;
+    }
+    if (AxisTwoInfo[HIGH_SPEED_RATIO].value != HighSpeedRatio[1])
+    {
+        AxisTwoInfo[HIGH_SPEED_RATIO].value = HighSpeedRatio[1];
+        AxisTwoInfoHasChanged = true;
+    }
+    if (AxisTwoInfo[PE_PERIOD].value != PESteps[1])
+    {
+        AxisTwoInfo[PE_PERIOD].value = PESteps[1];
+        AxisTwoInfoHasChanged = true;
+    }
+    if (AxisTwoInfoHasChanged && InformClient)
+        IDSetNumber(&AxisTwoInfoV, NULL);
+
+    bool AxisTwoStateHasChanged = false;
+    if (AxisTwoState[FULL_STOP].s != (AxesStatus[1].FullStop ? ISS_ON : ISS_OFF))
+    {
+        AxisTwoState[FULL_STOP].s = AxesStatus[1].FullStop ? ISS_ON : ISS_OFF;
+        AxisTwoStateHasChanged = true;
+    }
+    if (AxisTwoState[SLEWING].s != (AxesStatus[1].Slewing ? ISS_ON : ISS_OFF))
+    {
+        AxisTwoState[SLEWING].s = AxesStatus[1].Slewing ? ISS_ON : ISS_OFF;
+        AxisTwoStateHasChanged = true;
+    }
+    if (AxisTwoState[SLEWING_TO].s != (AxesStatus[1].SlewingTo ? ISS_ON : ISS_OFF))
+    {
+        AxisTwoState[SLEWING_TO].s = AxesStatus[1].SlewingTo ? ISS_ON : ISS_OFF;
+        AxisTwoStateHasChanged = true;
+    }
+    if (AxisTwoState[SLEWING_FORWARD].s != (AxesStatus[1].SlewingForward ? ISS_ON : ISS_OFF))
+    {
+        AxisTwoState[SLEWING_FORWARD].s = AxesStatus[1].SlewingForward ? ISS_ON : ISS_OFF;
+        AxisTwoStateHasChanged = true;
+    }
+    if (AxisTwoState[HIGH_SPEED].s != (AxesStatus[1].HighSpeed ? ISS_ON : ISS_OFF))
+    {
+        AxisTwoState[HIGH_SPEED].s = AxesStatus[1].HighSpeed ? ISS_ON : ISS_OFF;
+        AxisTwoStateHasChanged = true;
+    }
+    if (AxisTwoState[NOT_INITIALISED].s != (AxesStatus[1].NotInitialized ? ISS_ON : ISS_OFF))
+    {
+        AxisTwoState[NOT_INITIALISED].s = AxesStatus[1].NotInitialized ? ISS_ON : ISS_OFF;
+        AxisTwoStateHasChanged = true;
+    }
+    if (AxisTwoStateHasChanged && InformClient)
+            IDSetSwitch(&AxisTwoStateV, NULL);
+
+    bool EncoderValuesHasChanged = false;
+    if (EncoderValues[0].value != Positions[0])
+    {
+        EncoderValues[0].value = Positions[0];
+        EncoderValuesHasChanged = true;
+    }
+    if (EncoderValues[1].value != Positions[1])
+    {
+        EncoderValues[1].value = Positions[1];
+        EncoderValuesHasChanged = true;
+    }
+    if (EncoderValuesHasChanged && InformClient)
+        IDSetNumber(&EncoderValuesV, NULL);
+
+}
+
--- branches/libindi-align/drivers/telescope/skywatcherAPIMount.h
+++ branches/libindi-align/drivers/telescope/skywatcherAPIMount.h
@@ -20,6 +20,7 @@
 
 #include "skywatcherAPI.h"
 
+
 class SkywatcherAPIMount : public SkywatcherAPI, public INDI::Telescope, public INDI::AlignmentSubsystem::AlignmentSubsystemForDrivers
 {
 public:
@@ -33,6 +34,7 @@ public:
     virtual bool ISNewBLOB (const char *dev, const char *name, int sizes[], int blobsizes[], char *blobs[], char *formats[], char *names[], int n);
     virtual bool initProperties();
     virtual void ISGetProperties (const char *dev);
+    virtual bool updateProperties();
     virtual const char *getDefaultName();
     virtual bool Connect();
     virtual bool ReadScopeStatus();
@@ -48,10 +50,32 @@ private:
     int skywatcher_tty_read(int fd, char *buf, int nbytes, int timeout, int *nbytes_read);
     int skywatcher_tty_write(int fd, const char * buffer, int nbytes, int *nbytes_written);
 
-    // Ovverides for the pure virtual functions in
+    void UpdateDetailedMountInformation(bool InformClient);
 
     // Custom debug level
     unsigned int DBG_SCOPE;
+
+    // Properties
+
+    static const char* DetailedMountInfoPage;
+    enum { MOTOR_CONTROL_FIRMWARE_VERSION, MOUNT_CODE, IS_DC_MOTOR };
+    INumber BasicMountInfo[3];
+    INumberVectorProperty BasicMountInfoV;
+    enum { MT_EQ6, MT_HEQ5, MT_EQ5, MT_EQ3, MT_GT, MT_MF, MT_114GT, MT_DOB, MT_UNKNOWN };
+    ISwitch MountType[9];
+    ISwitchVectorProperty MountTypeV;
+    enum { GEAR_RATIO, STEP_TIMER_FREQUENCY, HIGH_SPEED_RATIO, PE_PERIOD };
+    INumber AxisOneInfo[4];
+    INumberVectorProperty AxisOneInfoV;
+    INumber AxisTwoInfo[4];
+    INumberVectorProperty AxisTwoInfoV;
+    enum { FULL_STOP, SLEWING, SLEWING_TO, SLEWING_FORWARD, HIGH_SPEED, NOT_INITIALISED };
+    ISwitch AxisOneState[6];
+    ISwitchVectorProperty AxisOneStateV;
+    ISwitch AxisTwoState[6];
+    ISwitchVectorProperty AxisTwoStateV;
+    INumber EncoderValues[2];
+    INumberVectorProperty EncoderValuesV;
 };
 
 #endif // SKYWATCHERAPIMOUNT_H
