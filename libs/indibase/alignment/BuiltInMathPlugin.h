29071b530361a58f43bd713e81de29cb5930c693
rogerjames99 <rogerjames99@14e3e35b-4bd0-4918-bbf4-08149c233275>
2014-01-13 20:47:29 +0000

    Slow and steady progress towards a working built in math plugin

--- libs/indibase/alignment/BuiltInMathPlugin.h
+++ libs/indibase/alignment/BuiltInMathPlugin.h
@@ -4,6 +4,8 @@
 #include "AlignmentSubsystemForMathPlugins.h"
 #include "ConvexHull.h"
 
+#include <gsl/gsl_matrix.h>
+
 namespace INDI {
 namespace AlignmentSubsystem {
 
@@ -20,9 +22,17 @@ public:
     virtual bool TransformTelescopeToCelestial(const TelescopeDirectionVector& TelescopeDirectionVector, double& RightAscension, double& Declination);
 
 private:
+    void CalculateTAKIMatrices(const TelescopeDirectionVector& Actual1, const TelescopeDirectionVector& Actual2, const TelescopeDirectionVector& Actual3,
+                            const TelescopeDirectionVector& Apparent1, const TelescopeDirectionVector& Apparent2, const TelescopeDirectionVector& Apparent3,
+                            gsl_matrix *pActualToApparent, gsl_matrix *pApparentToActual);
+
     // Offsets for single point alignment
     ln_hrz_posn SinglePointOffsetsAltAz;
     ln_equ_posn SinglePointsOffsetsRaDec;
+
+    // Transformation matrixes for 2 and 2 sync points case
+    gsl_matrix *pActualToApparentTransform;
+    gsl_matrix *pApparentToActualTransform;
 };
 
 } // namespace AlignmentSubsystem
