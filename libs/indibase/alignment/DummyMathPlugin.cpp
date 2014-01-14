91daad7d6540139cf59cc92636fa337b70564170
rogerjames99 <rogerjames99@14e3e35b-4bd0-4918-bbf4-08149c233275>
2014-01-14 14:17:45 +0000

    More files I forgot

---
+++ libs/indibase/alignment/DummyMathPlugin.cpp
@@ -0,0 +1,52 @@
+#include "DummyMathPlugin.h"
+
+namespace INDI {
+namespace AlignmentSubsystem {
+
+
+// Standard functions required for all plugins
+extern "C"
+{
+    DummyMathPlugin* Create()
+    {
+        return new DummyMathPlugin;
+    }
+
+    void Destroy(DummyMathPlugin *pPlugin)
+    {
+        delete pPlugin;
+    }
+
+    const char *GetDisplayName()
+    {
+        return "Dummy Math Plugin";
+    }
+}
+
+DummyMathPlugin::DummyMathPlugin()
+{
+    //ctor
+}
+
+DummyMathPlugin::~DummyMathPlugin()
+{
+    //dtor
+}
+
+bool DummyMathPlugin::Initialise()
+{
+    return false;
+}
+
+bool DummyMathPlugin::TransformCelestialToTelescope(const double RightAscension, const double Declination, TelescopeDirectionVector& TelescopeDirectionVector)
+{
+    return false;
+}
+
+bool DummyMathPlugin::TransformTelescopeToCelestial(const TelescopeDirectionVector& TelescopeDirectionVector, double& RightAscension, double& Declination)
+{
+    return false;
+}
+
+} // namespace AlignmentSubsystem
+} // namespace INDI
